90a9fa91586af33b0553fbf8ecff45ab27154cb2
antoninapashova <95548323+antoninapashova@users.noreply.github.com>
2021-12-05 21:27:41 +0200

    Create photography.js

---
+++ src/main/resources/static/app/photography.js
@@ -0,0 +1,20 @@
+document.getElementById('photographyBtn').addEventListener('click', () => {
+    fetch(`http://localhost:8080/photography`)
+        .then(res => res.json())
+        .then(data => data.forEach(el => {
+            let div = document.getElementById('div');
+            let divCard = document.createElement('div');
+            divCard.classList.add('card')
+            divCard.innerHTML = `<div class="col-sm-12 col-md-7 col-lg-5">
+               <img src=${el.photo.url} class="card-img-top" alt="Card image cap">
+                         <div class="card-body">
+                          <h5 class="card-user">${el.user.username}</h5>
+                          <h5  class="card-title">${el.title}</h5>
+                          <h5  class="card-title">${el.category.category}</h5>
+                          <p class="card-text">${el.description}</p>
+                    </div>
+                </div>`;
+
+            div.appendChild(divCard);
+        }))
+})
